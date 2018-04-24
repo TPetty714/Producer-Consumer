1bfced7646acd837c910d2461de220e409f48734
Dennis_Vargas <dennis.vargas@wsu.edu>
2018-04-23 21:24:23 -0700

    Renames many variables to match textbook specifications.

--- main.c
+++ main.c
@@ -11,34 +11,39 @@
 #include "buffer.h"
 
 pthread_mutex_t buff_mutex;
+pthread_cond_t empty;
+pthread_cond_t full;
+
 char* ShareMalloc(int);
 
 
-BufferItem buffer [BUFFER_SIZE];
+BufferItem itemBuffer [BUFFER_SIZE];
 
 void *Consumer(void *param);
 int InsertItem(BufferItem item);
 void *Producer(void *param);
 int RemoveItem(BufferItem *item);
 int Power(int base, int exp);
+pthread_t *producerThreads;
+pthread_t *consumerThreads;
 
-void *Consumer(void *param){
-    BufferItem item;
-    while(true){
-//        sleep for a random period of time
-//        sleep();
-        if(RemoveItem(&item)){
-            fprintf(stderr,"remove item error");
-        }else{
-            printf("consumer consumed %d\n", item);
-        }
-    }
-}
+//void *Consumer(void *param){
+//    BufferItem item;
+//    while(true){
+////        sleep for a random period of time
+////        sleep();
+//        if(RemoveItem(&item)){
+//            fprintf(stderr,"remove item error");
+//        }else{
+//            printf("consumer consumed %d\n", item);
+//        }
+//    }
+//}
 
 int InsertItem(BufferItem item){
 		// insert item into buffer
 		// return 0 if successful, otherwise
-	printf("Adding %d to buffer\n", item);
+//	printf("Adding %d to buffer\n", item);
 	return 0;
 		// return -1 indicating an error condition
 }
@@ -56,7 +61,8 @@ int Exit(pthread_t *producers, int num_prod, pthread_t *consumers, int num_consu
 	return 0;
 }
 
-void Producer(int prodId) {
+void *Producer(void* prodId) {
+    int randSleep = rand();
 	while(1) {
 		while (pthread_mutex_lock(&buff_mutex) != 0)
 			fprintf(stderr, "ERROR on pthread_mutex_lock. producerID: %d, errno:%d ", prodId, errno);
@@ -66,7 +72,7 @@ void Producer(int prodId) {
 	}
 }
 
-void Consumer(int conId) {
+void *Consumer(void *conId) {
 	while(1) {
 		while (pthread_mutex_lock(&buff_mutex) != 0)
 			fprintf(stderr, "ERROR on pthread_mutex_lock. consumerID: %d, errno:%d ", conId + 1, errno);
@@ -81,63 +87,69 @@ int main(int argc,char** argv)
 //    1. get command line arguments argv[1], argv[2], argv[3]
     char* c = argv[1];
     int sleepTime = 0;
-    int producerSize = 0;
-    int consumerSize = 0;
+    int numProdThreads = 0;
+    int numConsThreads = 0;
 
     sscanf(argv[1],"%d", &sleepTime);
-    sscanf(argv[2], "%d", &producerSize);
-    sscanf(argv[3], "%d", &consumerSize);
+    sscanf(argv[2], "%d", &numProdThreads);
+    sscanf(argv[3], "%d", &numConsThreads);
 
     printf("sleeptime: %d \n", sleepTime);
-    printf("producerSize: %d\n", producerSize);
-    printf("producerSize: %d\n", consumerSize);
+    printf("producerSize: %d\n", numProdThreads);
+    printf("producerSize: %d\n", numConsThreads);
 
 //    sleep time, number producers, number consumers
 
-	int num_prod = 10;
-	int num_consum = 10;
 //    2. initialize buffer
 
 //    2a. initialize mutex for buffer
 	pthread_mutex_init(&buff_mutex, NULL);
 //    3. create producer thread(s)
-	pthread_t *producer_array = malloc(sizeof(pthread_t)*num_prod);
-	for (int i = 0; i < num_prod; i++)
-		if(pthread_create(&producer_array[i],NULL, (void*)Producer, (int*)i)!=0)
-			fprintf(stderr, "pthread failed to create errno: %d producerID: %d",errno, i);
-		else
-			printf("Producer %d Producing\n", i+1);
+	initProducerThreads(numProdThreads);
+
 //    4. create consumer thread(s)
-	pthread_t *consumer_array = malloc(sizeof(pthread_t)*num_consum);
-	for (int i = 0; i < num_consum; i++)
-		if(pthread_create(&consumer_array[i],NULL, (void*)Consumer, (int*)i)!=0)
-			fprintf(stderr, "pthread failed to create errno: %d consumerID: %d",errno, i);
-		else
-			printf("Consumer %d consuming\n", i+1);
-	sleep(100);
+	initConsumerThreads(numConsThreads);
+
 //    5. sleep
+    sleep(100);
 //    6. Exit
-	Exit(*producer_array, num_prod, *consumer_array, num_consum);
+	Exit(producerThreads, numProdThreads, consumerThreads, numConsThreads);
 	return 0;
 }
 
-    return 0;
-}
-
-void *Producer(void *param){
-    BufferItem item;
-    while(true){
-//        sleep for a random period of time
-//        sleep();
-//        generate a random number
-        item = rand();
-        if(InsertItem(item)){
-            fprintf(stderr, "report error");
-        }else{
-            printf("producer produced %d\n", item);
-        }
+void initProducerThreads(int numThreads){
+    producerThreads = malloc(sizeof(pthread_t)*numThreads);
+    for (int i = 0; i < numThreads; i++){
+        if(pthread_create(&producerThreads[i],NULL, Producer, &i)!=0)
+            fprintf(stderr, "pthread failed to create errno: %d producerID: %d",errno, i);
+        else
+            printf("Producer %d Producing\n", i+1);
     }
 }
+void initConsumerThreads(int numThreads){
+    consumerThreads = malloc(sizeof(pthread_t)*numThreads);
+    for (int i = 0; i < numThreads; i++){
+        if(pthread_create(&consumerThreads[i],NULL, Consumer, &i)!=0)
+            fprintf(stderr, "pthread failed to create errno: %d consumerID: %d",errno, i);
+        else
+            printf("Consumer %d consuming\n", i+1);
+    }
+}
+
+//void *Producer(void *param){
+//    BufferItem item;
+//    while(true){
+////        sleep for a random period of time
+////        sleep();
+////        generate a random number
+//        item = rand();
+//        if(InsertItem(item)){
+//            fprintf(stderr, "report error");
+//        }else{
+//            printf("producer produced %d\n", item);
+//        }
+//    }
+//}
 
 
 int RemoveItem(BufferItem *item){
